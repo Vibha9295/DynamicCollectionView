a148c49f3be428b3dcd0b778b301e4e28f67ba8d
Vibha9295 <vachhani.vibha95@gmail.com>
2018-10-05 19:22:24 +0530

    Add files via upload

---
+++ demoCollectionWithDynamicSize/demoCollectionWithDynamicSize/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  demoCollectionWithDynamicSize
+//
+//  Created by vibha on 05/10/18.
+//  Copyright © 2018 vibha. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout,UICollectionViewDelegate {
+    
+    @IBOutlet var widthCell: NSLayoutConstraint!
+    @IBOutlet var collectionView: UICollectionView!
+    let randomTexts = ["Aenean dapibus urna a ullamcorper malesuada. Ut tempor.",
+                       "Sed venenatis ligula massa, a vulputate ipsum fringilla eget. Ut justo erat, facilisis id rhoncus cursus, fringilla at.",
+                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lobortis nibh metus, elementum tempus libero ornare vitae. Etiam sed leo pretium, consectetur turpis non, dapibus purus. Suspendisse potenti. Ut ut eros nunc. Cras nulla justo, porttitor non sapien at, iaculis.",
+                       "Maecenas pellentesque sed magna in congue. Sed non lacus in mi posuere scelerisque. Aenean.",
+                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget ex a velit tincidunt sodales. Donec elementum nisi at enim tempus, et rutrum erat semper. Phasellus ultricies est nec finibus.","Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lobortis nibh metus, elementum tempus libero ornare vitae. Etiam sed leo pretium, consectetur turpis non, dapibus purus. Suspendisse potenti. Ut ut eros nunc. Cras nulla justo, porttitor non sapien at, iaculis.",
+                       "Maecenas pellentesque sed magna in congue. Sed non lacus in mi posuere scelerisque. Aenean.",
+                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget ex a velit tincidunt sodales. Donec elementum nisi at enim tempus, et rutrum erat semper. Phasellus ultricies est nec finibus."]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Collectionview Methods
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return randomTexts.count;
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! Cell
+        cell.descriptionLabel.text = randomTexts[indexPath.row]
+        return cell
+    }
+    
+    func heightForView(text:String, font:UIFont, width:CGFloat) -> CGFloat {
+        let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = font
+        label.text = text
+        
+        label.sizeToFit()
+        return label.frame.height
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let Size: CGFloat = CGFloat(calculateHeight(forLbl: randomTexts[indexPath.row], width: Float(view.frame.size.width / 2 - 20)))
+        return CGSize(width: view.frame.size.width / 2 - 20, height: Size + 30)
+        
+//        return CGSize(width: (self.view.frame.width / 2) - 10, height: heightForView(text: randomTexts[indexPath.item], font: UIFont.systemFont(ofSize: 17), width: 160))
+    }
+    func calculateHeight(forLbl text: String?, width: Float) -> Float {
+        let constraint = CGSize(width: CGFloat(width), height: 20000.0)
+        var size: CGSize
+        
+        let context = NSStringDrawingContext()
+        let boundingBox: CGSize? = text?.boundingRect(with: constraint, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 15)], context: context).size
+        
+        size = CGSize(width: ceil((boundingBox?.width)!), height: ceil((boundingBox?.height)!))
+        
+        return Float(size.height + 10)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+}
+
